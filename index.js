3dcc2a5f0988b567315b74f65c520c6bd9dc3e04
Gonzalo Sánchez <gon.sanchez@example.es>
2023-12-04 10:25:30 +0100

    añado concesionarios

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
 // Importamos las bibliotecas necesarias.
 // Concretamente el framework express.
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 // Inicializamos la aplicación
 const app = express();
@@ -15,12 +17,91 @@ const app = express();
 // Indicamos que la aplicación puede recibir JSON (API Rest)
 app.use(express.json());
 
+// Configuración de Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // Indicamos el puerto en el que vamos a desplegar la aplicación
 const port = process.env.PORT || 8080;
 
 // Arrancamos la aplicación
 app.listen(port, () => {
   console.log(`Servidor desplegado en puerto: ${port}`);
+});
+
+// Definimos una estructura de datos
+// (temporal hasta incorporar una base de datos)
+let concesionarios = [
+  { nombre: "Lepi", direccion: "Calle Lepi", listadoCoches: coches },
+  { nombre: "Ferrari", direccion: "Calle Larios", listadoCoches: coches },
+  { nombre: "Porsche", direccion: "Calle Vireli", listadoCoches: coches },
+  { nombre: "Haul", direccion: "Calle Valdes", listadoCoches: coches },
+  { nombre: "Huaga", direccion: "Calle Huaga", listadoCoches: coches },
+];
+
+// Lista todos los concesionarios
+app.get("/concesionarios", (request, response) => {
+  response.json(concesionarios);
+});
+
+// Añadir un nuevo concesionario
+app.post("/concesionarios", (request, response) => {
+  concesionarios.push(request.body);
+  response.json({ message: "ok" });
+});
+
+// Obtener un solo concesionario
+app.get("/concesionarios/:id", (request, response) => {
+  const id = request.params.id;
+  const result = concesionarios[id];
+  response.json({ result });
+});
+
+// Actualizar un solo concesionario
+app.put("/concesionarios/:id", (request, response) => {
+  const id = request.params.id;
+  concesionarios[id] = request.body;
+  response.json({ message: "ok" });
+});
+
+// Borrar un elemento del array
+app.delete("/concesionario/:id", (request, response) => {
+  const id = request.params.id;
+  concesionarios = concesionarios.filter((item) => concesionarios.indexOf(item) !== id);
+
+  response.json({ message: "ok" });
+});
+
+// Devuelve todos los coches del concesionario pasado por id (solo los coches)
+app.get("/concesionarios/:id/coches", (request, response) => {
+  response.json(concesionarios);
+});
+
+// Añade un nuevo coche al concesionario pasado por id
+app.post("/concesionarios/:id/coches", (request, response) => {
+  concesionarios.push(request.body);
+  response.json({ message: "ok" });
+});
+
+// Obtiene el coche cuyo id sea cocheId, del concesionario pasado por id
+app.get("/concesionarios/:id/coches/:cocheId", (request, response) => {
+  const id = request.params.id;
+  const result = concesionarios[id];
+  response.json({ result });
+});
+
+// Actualiza el coche cuyo id sea cocheId, del concesionario pasado por id
+app.put("/concesionarios/:id/coches/:cocheId", (request, response) => {
+  const id = request.params.id;
+  concesionarios[id] = request.body;
+  response.json({ message: "ok" });
+});
+
+// Borra el coche cuyo id sea cocheId, del concesionario pasado por id
+app.delete("/concesionario/:id/coches/:cocheId", (request, response) => {
+  const id = request.params.id;
+  concesionarios = concesionarios.filter((item) => concesionarios.indexOf(item) !== id);
+
+  response.json({ message: "ok" });
 });
 
 // Definimos una estructura de datos
